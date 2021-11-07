66f4526b2903374a716252ba535e51693e0263f1
s0578026 <kay.birth@student.htw-berlin.de>
2021-11-07 12:45:12 +0100

    added ueberweisen

--- src/main/java/bankprojekt/verarbeitung/Bank.java
+++ src/main/java/bankprojekt/verarbeitung/Bank.java
@@ -101,6 +101,7 @@ public class Bank implements Bankfaehig{
     public boolean geldUeberweisen(long vonKontoNummer, long nachKontoNummer, double betrag, String verwendungszweck) throws GesperrtException {
         if (konten.containsKey(vonKontoNummer) && konten.containsKey(nachKontoNummer)){
             if(Ueberweisungsfaehig.class.isAssignableFrom(konten.get(vonKontoNummer).getClass()) && Ueberweisungsfaehig.class.isAssignableFrom(konten.get(nachKontoNummer).getClass())){
+                // Casten konnte nur Hardcoded gelöst werden
                 Girokonto vonKonto = (Girokonto) konten.get(vonKontoNummer);
                 Girokonto nachKonto = (Girokonto) konten.get(nachKontoNummer);
                 boolean absenden =vonKonto.ueberweisungAbsenden(betrag,nachKonto.getInhaber().getName(),nachKontoNummer,bankleitzahl,verwendungszweck);
